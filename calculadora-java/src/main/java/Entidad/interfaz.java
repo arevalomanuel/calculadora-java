83ebb6d574afefede434885e18991832bfb1725d
arevalomanuel <manuelarevalo373@gmail.com>
2023-10-15 02:06:09 -0300

    first commit

---
+++ calculadora-java/src/main/java/Entidad/interfaz.java
@@ -0,0 +1,19 @@
+package Entidad;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+
+public class interfaz extends JPanel {
+    private JButton  boton;
+    
+    public interfaz(){
+        teclado();
+    }
+    
+    public void teclado(){
+        boton = new JButton();
+        
+        
+    }
+}
