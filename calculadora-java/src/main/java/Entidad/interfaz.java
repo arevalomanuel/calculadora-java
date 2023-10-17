ddd6e08e5d85efea8e384905c3903da822709b53
arevalomanuel <manuelarevalo373@gmail.com>
2023-10-16 23:09:50 -0300

    nuevos ejercicios

--- calculadora-java/src/main/java/Entidad/interfaz.java
+++ calculadora-java/src/main/java/Entidad/interfaz.java
@@ -1,13 +1,17 @@
 package Entidad;
 
+import java.awt.Dimension;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 
 public class interfaz extends JPanel {
     private JButton  boton;
+    private final int L_HEIGHT = 500;
+    private final int L_WIDTH = 500;
     
     public interfaz(){
+        setPreferredSize(new Dimension(L_WIDTH, L_HEIGHT));
         teclado();
     }
     
