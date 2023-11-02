dff5685a1c162d66be413bf65c42123ae81183be
arevalomanuel <manuelarevalo373@gmail.com>
2023-11-01 22:13:41 -0300

    new

--- calculadora-java/src/main/java/Entidad/interfaz.java
+++ calculadora-java/src/main/java/Entidad/interfaz.java
@@ -3,50 +3,49 @@ package Entidad;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+public class interfaz extends JPanel implements ActionListener {
 
-public final class interfaz extends JPanel implements ActionListener{
     //numers
-    private JButton  numer_0;
-    private JButton  numer_1;
-    private JButton  numer_2;
-    private JButton  numer_3;
-    private JButton  numer_4;
-    private JButton  numer_5;
-    private JButton  numer_6;
-    private JButton  numer_7;
-    private JButton  numer_8;
-    private JButton  numer_9;
-    
-    //arimetics operators
-    private JButton  operatorDivision;
-    private JButton  operatorMore;
-    private JButton  operatorMultical;
-    private JButton  operadoresLow;
-    
-    //clear and equals
-    private JButton  clearButton;
-    private JButton  equalsButton;
-    
-    
+    private final List<JButton> keyPat = new ArrayList();
+
     private final int L_HEIGHT = 500;
     private final int L_WIDTH = 500;
-    
-    public interfaz(){
+
+    public interfaz() {
         setPreferredSize(new Dimension(L_WIDTH, L_HEIGHT));
-         numericKeypat();
+        numericKeypat();
     }
-    
-    public void numericKeypat(){
-        boton = new JButton();
-        
-        
+
+    public void numericKeypat() {
+       
+    }
+
+    private void createButtons() {
+         for (int i = 0; i < 24; i++) {
+            keyPat.add(createButton());
+        }
+    }
+
+    private JButton createButton() {
+        JButton boton = new JButton();
+        boton.setSize(50, 50);
+        return boton;
+    }
+
+    public static void main(String[] args) {
+        interfaz x = new interfaz();
+
+        x.createButton();
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+
 }
